d5148c76a0e79f9affb8fa2778b319d78a45b612
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-11-29 16:28:38 -0300

    edit base project and add components and imgs

---
+++ src/components/Image.js
@@ -0,0 +1,40 @@
+import "../stylesheets/Image.css";
+
+export function Image(props) {
+  const openImg = () => {
+    let imgModalOne = document.getElementsByClassName("img-modal")[0];
+    let imgBase = document.getElementsByClassName("gy-img")[0];
+    imgModalOne.style.display = "flex";
+    imgBase.classList.add("open");
+
+    if (imgBase.classList.contains("open")) {
+      let imgNew = document.createElement("img");
+      imgModalOne.appendChild(imgNew);
+      imgNew.src = require(`../img/img-${props.img}.${props.ext}`);
+      imgNew.className = "modal-content";
+    }
+  };
+
+  const closeImg = (e) => {
+    let imgModalOne = document.getElementsByClassName("img-modal")[0];
+    let imgBase = document.getElementsByClassName("gy-img")[0];
+    let imgNew = document.getElementsByClassName("modal-content");
+    imgModalOne.style.display = "none";
+    imgBase.classList.remove("open");
+    imgModalOne.remove(imgNew);
+  };
+  return (
+    <>
+      <div className="gy-item gy-item-1" onClick={openImg}>
+        <img
+          className="gy-img"
+          src={require(`../img/img-${props.img}.${props.ext}`)}
+          alt="digital art by Charly BGood"
+        />
+      </div>
+      <div className="img-modal" onClick={closeImg}></div>
+    </>
+  );
+}
+
+
