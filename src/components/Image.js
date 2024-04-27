d5678244659402d8930f2a4831d0115aa8c4d1b7
charlybgood <bonavita.carlos@gmail.com>
2024-04-27 18:40:55 -0300

    fix gallery cmp & add logic into img cmp - - change css

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,62 @@
 import "../stylesheets/VisualArt.css";
+import { useState } from "react";
+import { srcImgs } from "../db/imgDb";
 
 export function Image({ src, className }) {
+  const [imageSrc, setImageSrc] = useState(null);
+  const [isHidden, setIsHidden] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((currentIndex + 1) % srcImgs.length);
+    setIsHidden(isHidden);
+    console.log('isHidden')
+  };
+
+  const handlePrevious = () => {
+    const previousIndex = (currentIndex - 1 + srcImgs.length) % srcImgs.length;
+    setCurrentIndex(previousIndex);
+  };
+
+  const openImg = (e) => {
+    setImageSrc(e.target.src);
+    setIsHidden(!isHidden);
+  };
+
+  const closeImg = (e) => {
+    setIsHidden(isHidden);
+  };
+
   return (
-    <img src={src} className={className} alt="digital art by Charly BGood" />
+    <>
+      <div className="gy-item" onClick={openImg}>
+        <img
+          src={src}
+          // className={className}
+          className="gy-img"
+          alt="digital art by Charly BGood"
+        />
+      </div>
+      <div
+        className={isHidden ? "img-modal" : "img-modal-open"}
+        onClick={closeImg}
+      >
+        <button
+          className="prev"
+          onClick={handlePrevious}
+          aria-label="Previous image"
+        >
+          &#10094;
+        </button>
+        <img
+          className="modal-content"
+          src={imageSrc}
+          alt="digital art by Charly BGood"
+        />
+        <button className="next" onClick={handleNext} aria-label="Next image">
+          &#10095;
+        </button>
+      </div>
+    </>
   );
 }
--- src/db/imgDb.js
+++ src/db/imgDb.js
@@ -1,10 +1,23 @@
-import one from "../assets/imgs/1.gif";
+// import one from "../assets/imgs/1.gif";
 // import two from "./img/2.png";
-import three from "../assets/imgs/3.png";
-import four from "../assets/imgs/4.gif";
+// import three from "../assets/imgs/3.png";
+// import four from "../assets/imgs/4.gif";
 // import five from "./img/5.gif";
 // import six from "./imgs/6.png";
 // import seven from "./img/7.gif";
 
 // export const imgs = [one, two, three, four, five, six, seven];
-export const imgs = [one, three, four];
+// export const imgs = [one, three, four];
+
+export const srcImgs = [
+  "https://media.giphy.com/media/V75H9GKVV33eWvJjeo/giphy.gif",
+  "https://media.giphy.com/media/vuDebpxeqF95dy4k7v/giphy.gif",
+  "https://media1.giphy.com/media/exPvEKmXHb6dWiTgQv/giphy.gif",
+  "https://media.giphy.com/media/cLP818k392JZpIFATT/giphy.gif",
+  "https://media.giphy.com/media/RwRWKEIFp8rtwn3SpC/giphy.gif",
+  "https://media.giphy.com/media/RL0VClRYDycJtJtrnu/giphy.gif",
+  "https://media.giphy.com/media/xXI7vCRLLviHeO41TF/giphy.gif",
+  "https://media.giphy.com/media/6V7gDOure95uLJkwAg/giphy.gif",
+  "https://media.giphy.com/media/3o6gaUNh6CJD3rOQOk/giphy.gif",
+  "https://media3.giphy.com/media/PFQYIEQJdKxjabBIFE/giphy.gif",
+];
