5e0da7dd4588588da28b61572c945df2a87173a2
Carlos Bonavita <carlos.bonavita@dataart.com>
2024-04-29 14:08:02 -0300

    fix modal from rendering multiple times to just one when img clicked

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,35 +1,6 @@
 import "../stylesheets/VisualArt.css";
-import { useState } from "react";
-import { srcImgs } from "../db/imgDb";
 
-export function Image({ src }) {
-  const [imageSrc, setImageSrc] = useState(null);
-  const [isHidden, setIsHidden] = useState(true);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const imgLength = [...srcImgs];
-
-  const handleNext = () => {
-    setCurrentIndex((currentIndex + 1) % imgLength.length);
-    setIsHidden(!isHidden);
-    console.log(isHidden);
-  };
-
-  const handlePrevious = () => {    
-    const previousIndex = (currentIndex - 1 + srcImgs.length) % srcImgs.length;
-    setCurrentIndex(previousIndex);
-    console.log(previousIndex);
-  };
-
-  const openImg = (e) => {
-    setImageSrc(e.target.src);
-    setIsHidden(!isHidden);
-    console.log(isHidden);
-  };
-
-  const closeImg = () => {
-    setIsHidden(!isHidden);
-  };
-
+export function Image({ src, openImg }) {
   return (
     <>
       <div className="gy-item">
@@ -39,26 +10,6 @@ export function Image({ src }) {
           alt="digital art by Charly BGood"
           onClick={openImg}
         />
-      </div>
-      <div
-        className={isHidden ? "img-modal" : "img-modal-open"}
-        onClick={closeImg}
-      >
-        <button
-          className="prev"
-          onClick={handlePrevious}
-          aria-label="Previous image"
-        >
-          &#10094;
-        </button>
-        <img
-          className="modal-content"
-          src={imageSrc}
-          alt="digital art by Charly BGood"
-        />
-        <button className="next" onClick={handleNext} aria-label="Next image">
-          &#10095;
-        </button>
       </div>
     </>
   );
--- src/components/VisualArt.js
+++ src/components/VisualArt.js
@@ -2,17 +2,51 @@
 import { Image } from "./Image";
 import { srcImgs } from "../db/imgDb";
 import "../stylesheets/VisualArt.css";
+import { useState } from "react";
 
 function VisualArt() {
-  const imgLength = [...srcImgs];
+  const [imageSrc, setImageSrc] = useState(null);
+  const [isModalHidden, setIsModalHidden] = useState(true);
 
+  const openImg = (e) => {
+    setImageSrc(e.target.src);
+    setIsModalHidden(!isModalHidden);
+  };
+
+  const closeImg = () => {
+    setIsModalHidden(!isModalHidden);
+  };
   return (
     <div className="main-gy-container">
       <h1 className="collage-h1">VisualArts_Gallery</h1>
       <div className="gy-container">
-        {imgLength.map((img) => {
-          return <Image src={img} />;
+        {srcImgs.map((img, index) => {
+          return <Image key={index} src={img} openImg={openImg} />;
         })}
+      </div>
+      <div
+        className={isModalHidden ? "img-modal" : "img-modal-open"}
+        onClick={closeImg}
+      >
+        <button
+          className="prev"
+          // onClick={handlePrevious}
+          aria-label="Previous image"
+        >
+          &#10094;
+        </button>
+        <img
+          className="modal-content"
+          src={imageSrc}
+          alt="digital art by Charly BGood"
+        />
+        <button
+          className="next"
+          // onClick={handleNext}
+          aria-label="Next image"
+        >
+          &#10095;
+        </button>
       </div>
     </div>
   );
